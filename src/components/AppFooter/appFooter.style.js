bd20a213f26c3728990f340368cc38a699f1d332
Andrei Dutkovsky <andreidutkovsly@yandex.ru>
2023-04-04 10:21:00 +0300

    build app footer

---
+++ src/components/AppFooter/appFooter.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const WrapAppFooter = styled.div`
+  display: flex;
+  align-items: center;
+  height: 60px;
+  padding: 10px 16px;
+  box-shadow: rgb(204, 204, 204) 0px -1px 4px 0;
+  h2 {
+    padding-left: 13px;
+  }
+  .address {
+    margin: auto;
+    padding: 0 13px;
+  }
+`;
+
+export { WrapAppFooter };
